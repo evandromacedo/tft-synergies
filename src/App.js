dbbc86aba366078909be25504aab43479f0a0912
Evandro Macedo <evandro.hcm@gmail.com>
2019-08-14 23:29:49 -0300

    Install prettier and enzyme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 1;
+
   return (
     <div className="App">
       <header className="App-header">
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+
+it("renders without crashing", () => {
+  shallow(<App />);
+  expect(0).toBe(false);
 });
