c63197614578946a9d1fa6fc2163701c14274a8f
Evandro Macedo <evandro.hcm@gmail.com>
2019-08-16 02:43:42 -0300

    Install styled component and storybook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [firstCount, updateFirstCount] = useState(0);
-  const [secondCount, updateSecondCount] = useState(100);
+  // const [value, changeValue] = useState(0);
+  const [stateCount, dispatchCount] = useReducer(reducerCount, initialStateCount);
+  const [stateClick, dispatchClick] = useReducer(reducerClick, initialStateClick);
 
-  function handleFirstCounter() {
-    if (firstCount + 1 > 10) {
-      return updateFirstCount(0);
-    }
-
-    updateFirstCount(firstCount + 1);
-  }
+  // useEffect(() => {
+  //   fetch('https://solomid-resources.s3.amazonaws.com/blitz/tft/data/classes.json')
+  //     .then(response => response.json())
+  //     .then(console.log);
+  // }, []);
 
   return (
     <div className="App">
-      <div>
-        <p>First counter: {firstCount}</p>
-        <button onClick={handleFirstCounter}>Update First Counter</button>
-        <p>Second counter: {secondCount}</p>
-        <button onClick={() => updateSecondCount(secondCount + 1)}>
-          Update Second Counter
-        </button>
-      </div>
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <button
+        onClick={() => {
+          dispatchCount({ type: 'increment' });
+          dispatchClick({ type: 'increment' });
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatchCount({ type: 'decrement' });
+          dispatchClick({ type: 'increment' });
+        }}
+      >
+        Decrement
+      </button>
+      <p>State count: {stateCount.count}</p>
+      <p>State clicks: {stateClick.clicks}</p>
     </div>
   );
 }
+
+const initialStateCount = {
+  count: 0
+};
+
+const reducerCount = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 10 };
+    case 'decrement':
+      if (state.count - 10 <= 0) {
+        return { count: 0 };
+      }
+      return { count: state.count - 10 };
+    default:
+      throw new Error();
+  }
+};
+
+const initialStateClick = {
+  clicks: 0
+};
+
+const reducerClick = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { clicks: state.clicks + 1 };
+    case 'decrement':
+      return { clicks: state.clicks - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 export default App;
---
+++ stories/opa.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import App from '../src/App';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+storiesOf('App', module).add('teste', () => <App />);
