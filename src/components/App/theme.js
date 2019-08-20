abfffedfbdc312eddc8fa240c848a76461bdeb11
Evandro Macedo <evandro.hcm@gmail.com>
2019-08-20 16:49:50 -0300

    Remove unused files from create-react-app

---
+++ src/components/App/theme.js
@@ -0,0 +1,39 @@
+export const theme = {
+  // Colors
+  background1: '#000000',
+  background2: '#121212',
+  background3: '#343434',
+  background4: '#979797',
+  accentYellow: '#D8A75B',
+  borderLevel1: {
+    top: '#213042',
+    bottom: '#131C26'
+  },
+  borderLevel2: {
+    top: '#156831',
+    bottom: '#0A3518'
+  },
+  borderLevel3: {
+    top: '#1652A0',
+    bottom: '#092242'
+  },
+  borderLevel4: {
+    top: '#893088',
+    bottom: '#3E143D'
+  },
+  borderLevel5: {
+    top: '#B89D27',
+    bottom: '#3A310B'
+  },
+
+  // Typography
+  bold12px: 'font-size: 1.2rem; font-weight: bold; letter-spacing: .5px;',
+  bold16px: 'font-size: 1.6rem; font-weight: bold; letter-spacing: .5px;',
+  bold20px: 'font-size: 2rem; font-weight: bold; letter-spacing: .5px;',
+  sans14px: 'font-family: sans-serif; font-size: 1.4rem; font-weight: normal;',
+  sans14pxItalic:
+    'font-family: sans-serif; font-size: 1.4rem; font-weight: normal; font-style: italic;',
+  sans16pxItalic:
+    'font-family: sans-serif; font-size: 1.6rem; font-weight: normal; font-style: italic;',
+  textShadow1: 'text-shadow: 0 0 3px #000;'
+};
