d88c326b596c2fd1e4630adf828d2565623f8de2
Evandro Macedo <evandro.hcm@gmail.com>
2019-08-21 20:59:17 -0300

    Adds Favicon on Meta

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import Favicon from '../../static/img/Favicon.ico';
 
 /* This will be filled properly later */
 export default function Meta() {
   return (
     <Helmet>
       <meta charset="utf-8" />
-      {/* <link rel="shortcut icon" href="../favicon.ico" /> */}
+      <link rel="shortcut icon" href={Favicon} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta name="description" content="Web site created using create-react-app" />
