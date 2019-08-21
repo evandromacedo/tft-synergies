0cffcdd029b0621feb387b2c6947ad1f50c069ca
Evandro Macedo <evandro.hcm@gmail.com>
2019-08-21 16:50:58 -0300

    Moves manifest to /Meta

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -6,11 +6,11 @@ export default function Meta() {
   return (
     <Helmet>
       <meta charset="utf-8" />
-      <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+      {/* <link rel="shortcut icon" href="../favicon.ico" /> */}
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta name="description" content="Web site created using create-react-app" />
-      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <link rel="manifest" href="manifest.json" />
       <title>TFT Synergies</title>
     </Helmet>
   );
