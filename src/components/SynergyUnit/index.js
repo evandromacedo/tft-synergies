3f72e279ede49e8ccf30bf79fa637b86f6c5d12b
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-03 02:36:41 -0300

    Finish SynergyUnit logic

--- src/components/SynergyUnit/index.js
+++ src/components/SynergyUnit/index.js
@@ -1,57 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { NoSynergy } from '../SynergyIcon';
 import * as S from './styled';
+import * as SynergyIcon from '../SynergyIcon';
 
 export default function SynergiesUnit({ details, count = 0 }) {
   const { bonuses } = details;
   const hasSynergy = bonuses && count >= bonuses[0].needed;
 
-  function printSynergyProgress(hasSynergy, count, bonuses) {
-    // No synergy yet
-    if (!hasSynergy) {
-      return (
-        <>
-          {count} / {bonuses[0].needed}
-        </>
-      );
-    }
-
-    // At least 1 synergy
-    if (bonuses.length === 1) {
-      return count;
-    }
-
-    const progress = bonuses.map((bonus, index, array) => {
-      const isLastBonus = !(index + 1 < array.length);
-      const isActiveBonus =
-        count >= bonus.needed && (isLastBonus || count < array[index + 1].needed);
-
-      if (!isActiveBonus) {
-        return (
-          <S.Unable key={index}>
-            {bonus.needed}
-            {!isLastBonus && ' > '}
-          </S.Unable>
-        );
-      }
-
-      return (
-        <span key={index}>
-          {bonus.needed}
-          {!isLastBonus && <S.Unable> > </S.Unable>}
-        </span>
-      );
-    });
-
-    return progress;
-  }
-
   return (
     <S.Wrapper>
       <S.Icon>
-        {count === 0 && <NoSynergy />}
-        {count > 0 && <NoSynergy />}
+        {count === 0 && <SynergyIcon.NoSynergy />}
+        {count > 0 && getSynergyIcon(hasSynergy, count, details)}
       </S.Icon>
 
       <S.Count>
@@ -64,11 +24,7 @@ export default function SynergiesUnit({ details, count = 0 }) {
             <S.Quantity>{count}</S.Quantity>
             <div>
               <S.Name>{details.name}</S.Name>
-              <S.Progress>
-                {printSynergyProgress(hasSynergy, count, bonuses)}
-
-                {/* <S.Unable>3 / </S.Unable>6<S.Unable> / 9</S.Unable> */}
-              </S.Progress>
+              <S.Progress>{printSynergyProgress(hasSynergy, count, bonuses)}</S.Progress>
             </div>
           </>
         )}
@@ -81,3 +37,83 @@ SynergiesUnit.propTypes = {
   details: PropTypes.object,
   count: PropTypes.number
 };
+
+function printSynergyProgress(hasSynergy, count, bonuses) {
+  // No synergy yet
+  if (!hasSynergy) {
+    return (
+      <>
+        {count} / {bonuses[0].needed}
+      </>
+    );
+  }
+
+  // 1 synergy bonus
+  if (bonuses.length === 1) {
+    return count;
+  }
+
+  const progress = bonuses.map((bonus, index, array) => {
+    const isLastBonus = !(index + 1 < array.length);
+    const isActiveBonus =
+      count >= bonus.needed && (isLastBonus || count < array[index + 1].needed);
+
+    if (!isActiveBonus) {
+      return (
+        <S.Unable key={index}>
+          {bonus.needed}
+          {!isLastBonus && ' > '}
+        </S.Unable>
+      );
+    }
+
+    return (
+      <span key={index}>
+        {bonus.needed}
+        {!isLastBonus && <S.Unable> > </S.Unable>}
+      </span>
+    );
+  });
+
+  return progress;
+}
+
+function getSynergyIcon(hasSynergy, count, details) {
+  const { bonuses } = details;
+
+  // No synergy yet
+  if (!hasSynergy) {
+    return SynergyIcon[details.name](SynergyIcon.PARTIAL);
+  }
+
+  // 1 synergy bonus
+  if (bonuses.length === 1) {
+    return SynergyIcon[details.name](SynergyIcon.GOLD);
+  }
+
+  let activeBonusIndex = 0;
+
+  bonuses.some((bonus, index, array) => {
+    const isLastBonus = !(index + 1 < array.length);
+    const isActiveBonus =
+      count >= bonus.needed && (isLastBonus || count < array[index + 1].needed);
+
+    if (isActiveBonus) {
+      activeBonusIndex = index;
+      return true;
+    }
+
+    return false;
+  });
+
+  switch (activeBonusIndex) {
+    case 0:
+      return SynergyIcon[details.name](SynergyIcon.BRONZE);
+
+    case bonuses.length - 1:
+      return SynergyIcon[details.name](SynergyIcon.GOLD);
+
+    default:
+      return SynergyIcon[details.name](SynergyIcon.SILVER);
+  }
+}
--- src/components/SynergyUnit/stories.js
+++ src/components/SynergyUnit/stories.js
@@ -16,23 +16,24 @@ const detailsGuardian = {
   ]
 };
 
-const detailsBlademaster = {
-  key: 'blademaster',
-  name: 'Blademaster',
-  description: 'Blademasters have a 35% chance to strike additional times each attack.',
-  accentChampionImage: 'https://cdn.blitz.gg/blitz/centered/Fiora_Splash_Centered_0.jpg',
+const detailsDemon = {
+  key: 'demon',
+  name: 'Demon',
+  description:
+    'Demons basic attacks have a 40% chance to burn 20 mana from their target and return mana to the attacker.',
+  accentChampionImage: 'https://cdn.blitz.gg/blitz/centered/Aatrox_Splash_Centered_0.jpg',
   bonuses: [
     {
-      needed: 3,
-      effect: '1 additional strike'
+      needed: 2,
+      effect: 'Return 15 mana back to attacker'
+    },
+    {
+      needed: 4,
+      effect: 'Return 30 mana back to attacker'
     },
     {
       needed: 6,
-      effect: '2 additional strikes'
-    },
-    {
-      needed: 9,
-      effect: '4 additional strikes'
+      effect: 'Return 45 mana back to attacker'
     }
   ]
 };
@@ -40,7 +41,10 @@ const detailsBlademaster = {
 storiesOf('SynergiesUnit', module)
   .add('Default', () => <SynergiesUnit details={{}} />)
   .add('2 Guardian', () => <SynergiesUnit count={2} details={detailsGuardian} />)
-  .add('2 Blademaster', () => <SynergiesUnit count={2} details={detailsBlademaster} />)
-  .add('3 Blademaster', () => <SynergiesUnit count={3} details={detailsBlademaster} />)
-  .add('6 Blademaster', () => <SynergiesUnit count={6} details={detailsBlademaster} />)
-  .add('9 Blademaster', () => <SynergiesUnit count={9} details={detailsBlademaster} />);
+  .add('1 Demon', () => <SynergiesUnit count={1} details={detailsDemon} />)
+  .add('2 Demon', () => <SynergiesUnit count={2} details={detailsDemon} />)
+  .add('3 Demon', () => <SynergiesUnit count={3} details={detailsDemon} />)
+  .add('4 Demon', () => <SynergiesUnit count={4} details={detailsDemon} />)
+  .add('5 Demon', () => <SynergiesUnit count={5} details={detailsDemon} />)
+  .add('6 Demon', () => <SynergiesUnit count={6} details={detailsDemon} />)
+  .add('7 Demon', () => <SynergiesUnit count={7} details={detailsDemon} />);
