b390364e685e33e972a57158d3930a8ea1d806a2
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-13 15:42:07 -0300

    Creates getItems on API

--- src/API.js
+++ src/API.js
@@ -1,3 +1,5 @@
+import orderBy from 'lodash/orderBy';
+
 const getApiEndpoint = function(name) {
   return `https://solomid-resources.s3.amazonaws.com/blitz/tft/data/${name}.json`;
 };
@@ -5,6 +7,32 @@ const getApiEndpoint = function(name) {
 export function getChampions() {
   return fetch(getApiEndpoint('champions'))
     .then(response => response.json())
+    .then(response => {
+      const championsArray = Object.keys(response).map(key => response[key]);
+      const championsOrdered = orderBy(championsArray, ['cost', 'name']);
+      return championsOrdered;
+    })
+    .catch(error => alert('an error has ocurred'));
+}
+
+export function getItems() {
+  return fetch(getApiEndpoint('items'))
+    .then(response => response.json())
+    .then(response => {
+      const itemsKeys = [
+        'youmuusghostblade',
+        'knightsvow',
+        'frozenmallet',
+        'yuumi',
+        'bladeoftheruinedking',
+        'darkin'
+      ];
+
+      const itemsArray = Object.keys(response).map(key => response[key]);
+      const itemsArrayFiltered = itemsArray.filter(item => itemsKeys.includes(item.key));
+      const itemsArrayOrdered = orderBy(itemsArrayFiltered, ['name']);
+      return itemsArrayOrdered;
+    })
     .catch(error => alert('an error has ocurred'));
 }
 
