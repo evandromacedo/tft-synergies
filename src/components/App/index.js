c4b241e90121e9dc177b8dc2b754809e02738c1f
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-17 20:06:16 -0300

    Insert Context on App

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,5 @@
-import React, { useContext, useMemo, createContext } from 'react';
+import React from 'react';
 import theme from './theme';
-import useSynergies from '../../store';
 import { ThemeProvider } from 'styled-components';
 import { Layout, Wrapper, GlobalStyle } from './styled';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -8,34 +7,24 @@ import Meta from '../Meta';
 import Header from '../Header';
 import Home from '../../pages/Home';
 import About from '../../pages/About';
-
-const DispatchContext = createContext(null);
-const StoreContext = createContext(null);
+import Context from '../../pages/Context';
 
 export default function App() {
-  const { state, ...dispatch } = useSynergies();
-  const stateValue = useMemo(() => state, [state]);
-
   return (
-    <DispatchContext.Provider value={{ ...dispatch }}>
-      <StoreContext.Provider value={stateValue}>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          <Layout>
-            <Router>
-              <Meta />
-              <Header />
-              <Wrapper>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" component={About} />
-              </Wrapper>
-            </Router>
-          </Layout>
-        </ThemeProvider>
-      </StoreContext.Provider>
-    </DispatchContext.Provider>
+    <Context>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Router>
+            <Meta />
+            <Header />
+            <Wrapper>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+            </Wrapper>
+          </Router>
+        </Layout>
+      </ThemeProvider>
+    </Context>
   );
 }
-
-export const useDispatch = () => useContext(DispatchContext);
-export const useStore = () => useContext(StoreContext);
