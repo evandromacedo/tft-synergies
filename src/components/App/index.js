2a60f9afcb08b57987361878f5a4d8ab9c34c8fb
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-17 17:46:59 -0300

    Sets context provider for state and dispatch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, { useContext, useMemo, createContext } from 'react';
+import theme from './theme';
+import useSynergies from '../../store';
+import { ThemeProvider } from 'styled-components';
 import { Layout, Wrapper, GlobalStyle } from './styled';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import { getChampions } from '../../API';
-// import useSynergies from '../../data';
-import theme from './theme';
-import { ThemeProvider } from 'styled-components';
 import Meta from '../Meta';
 import Header from '../Header';
 import Home from '../../pages/Home';
 import About from '../../pages/About';
 
+const DispatchContext = createContext(null);
+const StoreContext = createContext(null);
+
 export default function App() {
-  // const { state, addChampion } = useSynergies();
+  const { state, ...dispatch } = useSynergies();
+  const stateValue = useMemo(() => state, [state]);
 
   return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Router>
-            <Meta />
-            <Header />
-            <Wrapper>
-              <Route path="/" exact component={Home} />
-              <Route path="/about" component={About} />
-            </Wrapper>
-          </Router>
-        </Layout>
-      </ThemeProvider>
-    </>
+    <DispatchContext.Provider value={{ ...dispatch }}>
+      <StoreContext.Provider value={stateValue}>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Router>
+              <Meta />
+              <Header />
+              <Wrapper>
+                <Route path="/" exact component={Home} />
+                <Route path="/about" component={About} />
+              </Wrapper>
+            </Router>
+          </Layout>
+        </ThemeProvider>
+      </StoreContext.Provider>
+    </DispatchContext.Provider>
   );
 }
+
+export const useDispatch = () => useContext(DispatchContext);
+export const useStore = () => useContext(StoreContext);
