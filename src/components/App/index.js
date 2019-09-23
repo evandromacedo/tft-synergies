564462ebba1fc155cff33053bb6f8763fe308ecc
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-23 13:04:03 -0300

    Reorder imports on App.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import theme from './theme';
 import isTouchDevice from '../../utils/isTouchDevice';
 import { ThemeProvider } from 'styled-components';
-import { Layout, Wrapper, GlobalStyle } from './styled';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
+import { Layout, Wrapper, GlobalStyle } from './styled';
 import Context from '../Context';
 import Meta from '../Meta';
 import Header from '../Header';
 import Home from '../../pages/Home';
 import About from '../../pages/About';
 import Footer from '../Footer';
-///////////////////////////
-import { DndProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
-import TouchBackend from 'react-dnd-touch-backend';
 
 const DndBackend = isTouchDevice() ? TouchBackend : HTML5Backend;
 
