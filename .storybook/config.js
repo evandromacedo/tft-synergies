b5ace3c9dbf97ecbcb281390d08145c563169445
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-23 12:51:47 -0300

    Adds DndProvider on config of storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,6 +5,8 @@ import { Layout, GlobalStyle } from '../src/components/App/styled';
 import { BrowserRouter as Router } from 'react-router-dom';
 import theme from '../src/components/App/theme';
 import Context from '../src/components/Context';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /(\.)?stories\.js$/);
@@ -16,14 +18,14 @@ const ThemeDecorator = storyFn => {
   return (
     <Context>
       <ThemeProvider theme={theme}>
-        <>
+        <DndProvider backend={HTML5Backend}>
           <GlobalStyle />
           <Layout>
             <Router>
               <div style={{ padding: '16px' }}>{storyFn()}</div>
             </Router>
           </Layout>
-        </>
+        </DndProvider>
       </ThemeProvider>
     </Context>
   );
