60cc0a643be817b82116fdbbbe4d4e1dc7f90326
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-23 23:48:05 -0300

    Dispatches on CurrentLevel

--- src/components/CurrentLevel/index.js
+++ src/components/CurrentLevel/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import * as S from './styled';
 import Plus from '../../static/icons/Plus12px.svg';
 import Minus from '../../static/icons/Minus12px.svg';
+import { useStore, useDispatch } from '../Context';
 
-export default function CurrentLevel({ level = 9, onLevelUp, onLevelDown }) {
+export default function CurrentLevel() {
+  const { level } = useStore();
+  const { levelUp, levelDown } = useDispatch();
+
   return (
     <S.Wrapper>
       <S.Text>Current Level:</S.Text>
-      <S.Button onClick={onLevelDown}>
+      <S.Button onClick={levelDown}>
         <img src={Minus} alt="Level Down" />
       </S.Button>
       <S.Text>{level}</S.Text>
-      <S.Button onClick={onLevelUp}>
+      <S.Button onClick={levelUp}>
         <img src={Plus} alt="Level Up" />
       </S.Button>
     </S.Wrapper>
