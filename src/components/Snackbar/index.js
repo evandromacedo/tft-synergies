6406e53bdae2cab884bb4f9a4a5e947b041176c4
Evandro Macedo <evandro.hcm@gmail.com>
2019-09-27 15:57:19 -0300

    Creates Snackbar component

---
+++ src/components/Snackbar/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Snackbar() {
+  return <h1>Snackbar</h1>;
+}
---
+++ src/components/Snackbar/stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Snackbar from './index.js';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Snackbar', module).add('Snackbar', () => <Snackbar />);
