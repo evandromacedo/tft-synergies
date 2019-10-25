704356325ab261952dbc27904028766d799c440a
Evandro Macedo <evandro.hcm@gmail.com>
2019-10-24 21:35:17 -0300

    Change about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  margin: 2.4rem 0;
+  font-size: 3.6rem;
+  line-height: 4.4rem;
+  text-align: center;
+`;
 
 export default function Home() {
-  return <h1>About</h1>;
+  return <H1>Vou fazer mais porra nenhuma nessa merda não!</H1>;
 }
